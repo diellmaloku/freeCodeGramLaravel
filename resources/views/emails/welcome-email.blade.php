500363a4ced6b791cce917fe6bef6949e54307ac
Diell Maloku <diellmaloku7@gmail.com>
2020-12-08 00:05:48 +0100

    Last changes

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Welcome to Diell's instagram lookalike App
+
+Prsh miqsia ime e dashtun.
+
+@component('mail::button', ['url' => 'https://www.facebook.com/diell.maloku'])
+Button Text
+@endcomponent
+
+Thanks,<br>
+Diell Mreti
+@endcomponent
